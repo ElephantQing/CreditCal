c85ddaafc859bc3ddab6562eefa258b37cf7903b
xq19830123 <xq1983@gmail.com>
2015-07-13 23:40:58 +0800

    init CreditDBOpenHelper.java

---
+++ src/cn/sh/elephant/creditcal/dao/CreditDBOpenHelper.java
@@ -0,0 +1,14 @@
+package cn.sh.elephant.creditcal.dao;
+
+/**
+ * Created by HomeStudio on 2015/7/13.
+ */
+public class CreditDBOpenHelper {
+    public static final String KEY_YEAR="YEAR";
+    public static final String KEY_PERIODS="PEROPDS";
+    public static final String KEY_C_RATE="C_RATE";
+    public static final String KEY_B_RATE="B_RATE";
+    private static final String DATABASE_NAME = "CreditCal.db";
+    private static final String DATABASE_TABLE = "";
+
+}
